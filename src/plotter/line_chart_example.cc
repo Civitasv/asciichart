386caa3b6671d93df31d421973ad5bf326fa8f08
Soeren Grunewald <soeren.grunewald@desy.de>
2023-09-06 11:12:35 +0200

    Minor improvements to meson and examples (#8)
    * meson: Use subdirs to structure the project
    
    [Why]
    In the existing form it was not possible to build out of tree, since
    the path to the source file was wrong. Also installing the library
    was not possible.
    
    [How]
    Use the subdir feature of meson to split up tasks. Then fix things
    where they occur.
    
    Signed-off-by: Soeren Grunewald <soeren.grunewald@desy.de>
    
    * plotter: Move all function in namespace
    
    [Why]
    It is good practice to move global functions to an anonymous namspace
    so they are limited to the compilation unit and the compiler can tell
    us if they are unused.
    
    Signed-off-by: Soeren Grunewald <soeren.grunewald@desy.de>
    
    * plotter: Make examples more accessible
    
    [Why]
    It is quite tedious to change + compile + execute the file for every
    example we wanna test.
    
    [How]
    Accept arguments and use them to select the corresponding sample.
    We keep it as simple as possible, so no proper error handling.
    
    Signed-off-by: Soeren Grunewald <soeren.grunewald@desy.de>
    
    ---------
    
    Signed-off-by: Soeren Grunewald <soeren.grunewald@desy.de>

--- src/plotter/line_chart_example.cc
+++ src/plotter/line_chart_example.cc
@@ -6,101 +6,111 @@
 
 #include "ascii/ascii.h"
 
-void example1();
-void example2();
-void example3();
-void example_legend();
-void example_legend2();
-void animation();
+namespace {
 
-int main() {
-  // example1();
-  // example2();
-  // example3();
-  example_legend();
-  // example_legend2();
-  // animation();
-  return 0;
-}
-
-void example1() {
-  using namespace ascii;
-  std::vector<double> series;
-  for (int i = 0; i < 100; i += 2) {
-    series.push_back(15 * std::cos(i * (kPI * 8) / 120));
-  }
-
-  Asciichart asciichart(std::vector<std::vector<double>>{series});
-  std::cout << asciichart.type(Asciichart::LINE).height(6).Plot();
-}
-
-void example2() {
-  using namespace ascii;
-  std::vector<double> series;
-  std::vector<double> series2;
-  for (int i = 0; i < 100; i += 2) {
-    series.push_back(15 * std::cos(i * (kPI * 8) / 120));
-    series2.push_back(15 * std::sin(i * ((kPI * 4) / 100)));
-  }
-  Asciichart asciichart(std::vector<std::vector<double>>{series, series2});
-  std::cout << asciichart.type(Asciichart::LINE).height(6).Plot();
-}
-
-void example3() {
-  using namespace ascii;
-  std::vector<double> series;
-  for (int i = 0; i < 100; i += 2) {
-    series.push_back(3400 * std::cos(i * (kPI * 8) / 120));
-  }
-
-  Asciichart asciichart(std::vector<std::vector<double>>{series});
-  std::cout << asciichart.type(Asciichart::LINE).height(6).Plot();
-}
-
-void animation() {
-  using namespace ascii;
-  std::vector<double> series;
-  std::vector<double> series2;
-  int height = 6;
-  for (int i = 0; i < 100; i += 2) {
-    series.push_back(15 * std::cos(i * (kPI * 8) / 120));
-    series2.push_back(15 * std::sin(i * ((kPI * 4) / 100)));
-    Asciichart asciichart(std::vector<std::vector<double>>{series, series2});
-    if (i != 0) {
-      for (int j = 0; j <= height; j++) {
-        std::cout << "\033[A\033[2K"; // This is used for clear previous chart
-      }
-      std::this_thread::sleep_for(std::chrono::milliseconds(500));
+  void example1() {
+    using namespace ascii;
+    std::vector<double> series;
+    for (int i = 0; i < 100; i += 2) {
+      series.push_back(15 * std::cos(i * (kPI * 8) / 120));
     }
-    std::cout << asciichart.type(Asciichart::LINE)
-                     .show_legend(true)
-                     .height(height)
-                     .Plot();
+
+    Asciichart asciichart(std::vector<std::vector<double>>{series});
+    std::cout << asciichart.type(Asciichart::LINE).height(6).Plot();
+  }
+
+  void example2() {
+    using namespace ascii;
+    std::vector<double> series;
+    std::vector<double> series2;
+    for (int i = 0; i < 100; i += 2) {
+      series.push_back(15 * std::cos(i * (kPI * 8) / 120));
+      series2.push_back(15 * std::sin(i * ((kPI * 4) / 100)));
+    }
+    Asciichart asciichart(std::vector<std::vector<double>>{series, series2});
+    std::cout << asciichart.type(Asciichart::LINE).height(6).Plot();
+  }
+
+  void example3() {
+    using namespace ascii;
+    std::vector<double> series;
+    for (int i = 0; i < 100; i += 2) {
+      series.push_back(3400 * std::cos(i * (kPI * 8) / 120));
+    }
+
+    Asciichart asciichart(std::vector<std::vector<double>>{series});
+    std::cout << asciichart.type(Asciichart::LINE).height(6).Plot();
+  }
+
+  void animation() {
+    using namespace ascii;
+    std::vector<double> series;
+    std::vector<double> series2;
+    int height = 6;
+    for (int i = 0; i < 100; i += 2) {
+      series.push_back(15 * std::cos(i * (kPI * 8) / 120));
+      series2.push_back(15 * std::sin(i * ((kPI * 4) / 100)));
+      Asciichart asciichart(std::vector<std::vector<double>>{series, series2});
+      if (i != 0) {
+        for (int j = 0; j <= height; j++) {
+          std::cout << "\033[A\033[2K"; // This is used for clear previous chart
+        }
+        std::this_thread::sleep_for(std::chrono::milliseconds(500));
+      }
+      std::cout << asciichart.type(Asciichart::LINE)
+                       .show_legend(true)
+                       .height(height)
+                       .Plot();
+    }
+  }
+
+  void example_legend() {
+    using namespace ascii;
+    std::vector<double> series;
+    std::vector<double> series2;
+    for (int i = 0; i < 100; i += 2) {
+      series.push_back(15 * std::cos(i * (kPI * 8) / 120));
+      series2.push_back(15 * std::sin(i * ((kPI * 4) / 100)));
+    }
+    Asciichart asciichart(std::vector<std::vector<double>>{series, series2});
+    std::cout
+        << asciichart.type(Asciichart::LINE).show_legend(true).height(6).Plot();
+  }
+
+  void example_legend2() {
+    using namespace ascii;
+    std::vector<double> series;
+    std::vector<double> series2;
+    for (int i = 0; i < 100; i += 2) {
+      series.push_back(15 * std::cos(i * (kPI * 8) / 120));
+      series2.push_back(15 * std::sin(i * ((kPI * 4) / 100)));
+    }
+    Asciichart asciichart({{"A", series}, {"B", series2}});
+    std::cout
+        << asciichart.type(Asciichart::LINE).show_legend(true).height(6).Plot();
+  }
+
+  void usage(const char* p) {
+    std::cout << "usage: " << p << " [1-6]\n";
   }
 }
 
-void example_legend() {
-  using namespace ascii;
-  std::vector<double> series;
-  std::vector<double> series2;
-  for (int i = 0; i < 100; i += 2) {
-    series.push_back(15 * std::cos(i * (kPI * 8) / 120));
-    series2.push_back(15 * std::sin(i * ((kPI * 4) / 100)));
+int main(int argc, char* argv[])
+{
+  if (argc != 2) {
+    usage(argv[0]);
+    return 1;
   }
-  Asciichart asciichart(std::vector<std::vector<double>>{series, series2});
-  std::cout
-      << asciichart.type(Asciichart::LINE).show_legend(true).height(6).Plot();
-}
 
-void example_legend2() {
-  using namespace ascii;
-  std::vector<double> series;
-  std::vector<double> series2;
-  for (int i = 0; i < 100; i += 2) {
-    series.push_back(15 * std::cos(i * (kPI * 8) / 120));
-    series2.push_back(15 * std::sin(i * ((kPI * 4) / 100)));
+  switch (std::stol(argv[1])) {
+  case 1: example1(); break;
+  case 2: example2(); break;
+  case 3: example3(); break;
+  case 4: example_legend(); break;
+  case 5: example_legend2(); break;
+  case 6: animation(); break;
+  default: usage(argv[0]); return 1;
   }
-  Asciichart asciichart({{"A", series}, {"B", series2}});
-  std::cout
-      << asciichart.type(Asciichart::LINE).show_legend(true).height(6).Plot();
+
+  return 0;
 }
